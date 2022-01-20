9937b9db145ee10a69ba60a11be24e1a0537a403
hateahate <stepanpavlenko@icloud.com>
2022-01-20 11:57:27 +0300

    Обновление Notifylib v0.4
    1. Добавлена функция вывода уведомлений для авторизованных/неавторизованных пользователей
    2. Добавление логики к функции действия встроена в генерацию уведомления

--- Template/vft/js/notifylib.js
+++ Template/vft/js/notifylib.js
@@ -1,3 +1,7 @@
+// Notifylib for Vitaforest Platform
+// Release v0.4
+// Created by Stepan Pavlenko
+
 // Базовый контейнер уведомлений
 
 const notificationContainer = document.querySelector('.notification-container');
@@ -26,6 +30,7 @@ function createNotification(addClassName, actionName, notificationText, name) {
             '<div class="' + addClassName + ' popup__head">' + '<p class="' + addClassName + ' popup__title">' + name + '</p>' + '</div>' + '<p class="' + addClassName + ' popup__text">' + notificationText + '</p>' + '<div class="popup-actions">' + '<a class="popup__confirm ' + addClassName + '-confirm" href="#">' + actionName + '</a>' + '</div>' + '</div>';
         // Добавляем уведомление в контейнер 
         notificationContainer.appendChild(newElement);
+        logicNotification(addClassName);
     }
 }
 
@@ -52,20 +57,21 @@ function logicNotification(className) {
 
 // Функция возвращающая результат проверки авторизации пользователя
 
-function userAuthCheck(func) {
+function createNotificationNonAuth(addClassName, actionName, notificationText, name) {
     if (notificationAuthData.dataset.auth != 'true') {
-        func();
+        createNotification(addClassName, actionName, notificationText, name);
+    }
+}
+
+function createNotificationAuth(addClassName, actionName, notificationText, name) {
+    if (notificationAuthData.dataset.auth == 'true') {
+        createNotification(addClassName, actionName, notificationText, name);
     }
 }
 
 // Уведомление о необходимости регистрации
 createNotification('notify1', 'Ok', 'Ммммм, хуета', 'Тестовый ноти');
-// Логика текущего уведомления
-logicNotification('notify1');
-
-
-userAuthCheck(createNotification('notify2', 'Ok', 'Охуеть, вот это да, вот это кайф, вааааай', 'Оно работает и похуй'));
-logicNotification('notify2');
+createNotificationNonAuth('notify2', 'Ok', 'Охуеть, вот это да, вот это кайф, вааааай', 'Оно работает и похуй');
 
 
 
